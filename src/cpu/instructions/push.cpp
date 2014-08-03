e09ee9d74fa1e75c0506951e571c026badbcad20
Stuart Jones <stuart@wynjones1.com>
2014-08-03 20:20:53 +0100

    n

--- src/cpu/instructions/push.cpp
+++
@@ -1,9 +0,0 @@
-#include "cpu.h"
-
-void PUSH(struct cpu_state *state,
-		const enum ARG_TYPE arg0, const union REG_INPUT i0,
-		const enum ARG_TYPE arg1, const union REG_INPUT i1)
-{
-	reg16_t d = cpu_load_reg16(state, i0);
-	cpu_push(state, d);
-}
