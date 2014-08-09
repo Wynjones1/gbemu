84ace62b47da93ba739691f2526d33569eed4a78
Stuart Jones <stuart@wynjones1.com>
2014-08-10 00:50:19 +0100

    Added output disable define

--- src/common.c
+++ src/common.c
@@ -1,14 +1,9 @@
 #include "common.h"
 #include <stdarg.h>
+#include <signal.h>
 
 uint32_t g_cycles = 0;
 
-#ifndef OUTPUT_ERRORS
-	#define OUTPUT_ERRORS   1
-#endif
-#ifndef OUTPUT_WARNINGS
-	#define OUTPUT_WARNINGS 1
-#endif
 
 void common_error(const char *format, ...)
 {
@@ -17,7 +12,7 @@ void common_error(const char *format, ...)
 	va_start(arg_list, format);
 	vprintf(format, arg_list);
 	va_end(arg_list);
-	exit(-1);
+	raise(SIGABRT);
 #endif
 
 }
@@ -29,5 +24,26 @@ void common_warn(const char *format, ...)
 	va_start(arg_list, format);
 	vprintf(format, arg_list);
 	va_end(arg_list);
+#endif
+}
+
+void common_output(const char *format, ...)
+{
+#if OUTPUT_OUTPUT
+	static FILE *fp;
+	if(!fp)
+	{
+#ifdef OUTPUT_FILENAME
+		fp = fopen(OUTPUT_FILENAME, "w");
+#else
+		fp = stdout;
+#endif
+
+	}
+	va_list arg_list;
+	va_start(arg_list, format);
+	vfprintf(fp, format, arg_list);
+	va_end(arg_list);
+	fflush(fp);
 #endif
 }
--- src/common.h
+++ src/common.h
@@ -4,11 +4,28 @@
 #include <stdint.h>
 #include <signal.h>
 
+#ifndef OUTPUT_ERRORS
+	#define OUTPUT_ERRORS   1
+#endif
+#ifndef OUTPUT_WARNINGS
+	#define OUTPUT_WARNINGS 1
+#endif
+
+#ifndef OUTPUT_OUTPUT
+	#define OUTPUT_OUTPUT 1
+#endif
+
+#if 1
+#define OUTPUT_FILENAME "log.txt"
+#endif
+
 #define Error(M, ...) common_error("Error: %s:%d in %s:\n\t" M, __FILE__, __LINE__, __func__, ##__VA_ARGS__)
 #define Warning(M, ...) common_warn("Warning: %s:%d in %s:\n\t" M, __FILE__, __LINE__, __func__,##__VA_ARGS__)
+#define Output(M, ...) common_output("Output: %s:%d in %s: " M, __FILE__, __LINE__, __func__,##__VA_ARGS__)
 
 void common_error(const char *format, ...);
 void common_warn(const char *format, ...);
+void common_output(const char *format, ...);
 
 #define BIT_N(x, n) ((x >> n) & 0x1)
 #define SET_N(x, n)   x |= (1 << n)
