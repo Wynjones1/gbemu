1ec13808d56606aa4a2f92c5987eac7e5f200b8a
Stuart Jones <stuart@wynjones1.com>
2015-02-12 02:25:28 +0000

    Added second square channel

--- src/audio.c
+++ src/audio.c
@@ -23,9 +23,8 @@ typedef struct
 }sample_t;
 
 #define FREQ(x) (CPU_CLOCK_SPEED / (32 * ((float)(2048 - (x)))))
-static int16_t square1(float t)
+static int16_t square(float t, float freq, int volume)
 {
-    float freq = FREQ(g_audio->freq);
     float val  = sin(freq * 2 * PI * t);
     int16_t out;
     if(val < 0.0)
@@ -36,12 +35,20 @@ static int16_t square1(float t)
     {
         out = INT16_MAX / 10;
     }
-#if 0
-    static FILE *fp;
-    if(!fp) fp = fopen("plot.txt", "w");
-    fprintf(fp, "%f %f\n", SDL_GetTicks() / 1000.0, val / (float)(INT16_MAX));
-#endif
-    return (g_audio->volume * out) / 15;
+    return (volume * out) / 15;
+}
+static int16_t square1(float t)
+{
+    float freq = FREQ(g_audio->sq1.freq_msb << 8 | g_audio->sq1.freq_lsb);
+    int16_t out = square(t, freq, g_audio->sq1.volume);
+    return out;
+}
+
+static int16_t square2(float t)
+{
+    float freq = FREQ(g_audio->sq2.freq_msb << 8 | g_audio->sq2.freq_lsb);
+    int16_t out = square(t, freq, g_audio->sq2.volume);
+    return out;
 }
 
 static void fill_audio(void *udata, Uint8 *stream, int len)
@@ -53,9 +60,13 @@ static void fill_audio(void *udata, Uint8 *stream, int len)
     {
         float t = pos / (float)FREQUENCY;
         int16_t val = 0;
-        if(g_audio->en)
+        if(g_audio->sq1.en)
         {
             val += square1(t);
+        }
+        if(g_audio->sq2.en)
+        {
+            val += square2(t);
         }
         samples[i]= (sample_t){.left = val, .right = val};
         pos = (pos + 1) % FREQUENCY;
@@ -87,8 +98,26 @@ audio_t *audio_init(cpu_state_t *state)
 	return out;
 }
 
+
 reg_t audio_load(audio_t *audio, reg16_t addr)
 {
+    switch(addr)
+    {
+    #define X(field, msb, lsb) ENCODE(audio->sq1.field, msb, lsb) |
+        case 0xff10: return (NR10 0x80);
+        case 0xff11: return (NR11 0x3f);
+        case 0xff12: return (NR12 0x00);
+        case 0xff13: return (NR13 0xff);
+        case 0xff14: return (NR14 0xbf);
+    #undef X
+    #define X(field, msb, lsb) ENCODE(audio->sq2.field, msb, lsb) |
+        case 0xff15: return 0xff;
+        case 0xff16: return (NR21 0x3f);
+        case 0xff17: return (NR22 0x00);
+        case 0xff18: return (NR23 0xff);
+        case 0xff19: return (NR24 0xbf);
+    #undef X
+    }
     return 0;
 }
 
@@ -100,63 +129,93 @@ void audio_delete(audio_t *audio)
 
 void length_counter(audio_t *audio)
 {
-    if(audio->load_len && audio->len_en)
+    if(audio->sq1.load_len && audio->sq1.len_en)
     {
-        audio->load_len -= 1;
-        if(audio->load_len == 0) audio->en = 0;
+        audio->sq1.load_len -= 1;
+        if(audio->sq1.load_len == 0) audio->sq1.en = 0;
+    }
+    if(audio->sq2.load_len && audio->sq2.len_en)
+    {
+        audio->sq2.load_len -= 1;
+        if(audio->sq2.load_len == 0) audio->sq2.en = 0;
     }
 }
 
 void sweep(audio_t *audio)
 {
-#if 1
-    uint16_t shadow   = audio->freq;
+    uint16_t shadow   = audio->sq1.freq_msb << 8 | audio->sq1.freq_lsb;
     uint16_t new_freq = 0;
     static uint16_t count = 0;
-    if(audio->sweep_period)
+    if(audio->sq1.sweep_period)
     {
         count++;
-        if(count >= audio->sweep_period)
+        if(count >= audio->sq1.sweep_period)
         {
             count = 0;
-            if(audio->shift > 0 || audio->sweep_period > 0)
+            if(audio->sq1.shift > 0 || audio->sq1.sweep_period > 0)
             {
-                new_freq = (shadow >> audio->shift);
-                new_freq = audio->negate ? -new_freq : new_freq;
+                new_freq = (shadow >> audio->sq1.shift);
+                new_freq = audio->sq1.negate ? -new_freq : new_freq;
                 new_freq = shadow + new_freq;
-                if(new_freq >> 11) audio->en = 0;
-                audio->freq = new_freq;
+                if(new_freq >> 11) audio->sq1.en = 0;
+                audio->sq1.freq_lsb = DECODE(new_freq, 7, 0);
+                audio->sq1.freq_msb = DECODE(new_freq, 10, 8);
             }
         }
     }
-#else
-#endif
 }
 
 void envelope(audio_t *audio)
 {
-    static int counter = 0;
-    if(audio->en && audio->period)
     {
-        if(counter >= audio->period)
+        static int counter = 0;
+        if(audio->sq1.en && audio->sq1.period)
+        {
+            if(counter >= audio->sq1.period)
+            {
+                counter = 0;
+                if(audio->sq1.add_mode)
+                {
+                    audio->sq1.volume += 1;
+                    if(audio->sq1.volume > 15) audio->sq1.volume = 15;
+                }
+                else
+                {
+                    audio->sq1.volume -= 1;
+                    if(audio->sq1.volume < 0) audio->sq1.volume = 0;
+                }
+            }
+            counter++;
+        }
+        else
         {
             counter = 0;
-            if(audio->add_mode)
-            {
-                audio->volume += 1;
-                if(audio->volume > 15) audio->volume = 15;
-            }
-            else
-            {
-                audio->volume -= 1;
-                if(audio->volume < 0) audio->volume = 0;
-            }
         }
-        counter++;
     }
-    else
     {
-        counter = 0;
+        static int counter = 0;
+        if(audio->sq2.en && audio->sq2.period)
+        {
+            if(counter >= audio->sq2.period)
+            {
+                counter = 0;
+                if(audio->sq2.add_mode)
+                {
+                    audio->sq2.volume += 1;
+                    if(audio->sq2.volume > 15) audio->sq2.volume = 15;
+                }
+                else
+                {
+                    audio->sq2.volume -= 1;
+                    if(audio->sq2.volume < 0) audio->sq2.volume = 0;
+                }
+            }
+            counter++;
+        }
+        else
+        {
+            counter = 0;
+        }
     }
 }
 
@@ -194,35 +253,50 @@ void audio_simulate(audio_t *audio, int clk)
 void  audio_store(audio_t *audio, reg16_t addr, reg_t data)
 {
     //printf("%04X ", addr); common_print_binary(stdout, data, 8); printf("\n");
-    if(addr == 0xff10)
+    #define X(field, msb, lsb) audio->sq1.field = DECODE(data, msb, lsb);
+    switch(addr)
     {
-        audio->shift        = DOWNTO(data, 2, 0);
-        audio->negate       = DOWNTO(data, 3, 3);
-        audio->sweep_period = DOWNTO(data, 6, 4);
-    }
-    if(addr == 0xff11)
-    {
-        audio->load_len = 64 - DOWNTO(data, 5, 0);
-        audio->duty     = DOWNTO(data, 7, 6);
-    }
-    if(addr == 0xff12)
-    {
-        audio->period        = DOWNTO(data, 2, 0);
-        audio->add_mode      = DOWNTO(data, 3, 3);
-        audio->start_volume  = DOWNTO(data, 7, 4);
-    }
-    if(addr == 0xff13)
-    {
-        audio->freq = (DOWNTO(audio->freq, 10, 8) << 8) | data;
-    }
-    if(addr == 0xff14)
-    {
-        audio->freq   = (DOWNTO(data, 2, 0) << 8) | DOWNTO(audio->freq, 7, 0);
-        audio->len_en = DOWNTO(data, 6, 6);
-        if(BIT_N(data, 7))
-        {
-            audio->en = 1;
-            audio->volume = audio->start_volume;
-        }
+        case 0xff10:
+            NR10;
+            break;
+        case 0xff11:
+            NR11;
+            audio->sq1.load_len = 64 - audio->sq1.load_len;
+            break;
+        case 0xff12:
+            NR12;
+            break;
+        case 0xff13:
+            NR13;
+            break;
+        case 0xff14:
+            NR14;
+            if(DECODE(data, 7, 7))
+            {
+                audio->sq1.en = 1;
+                audio->sq1.volume = audio->sq1.start_volume;
+            }
+            break;
+    #undef X
+    #define X(field, msb, lsb) audio->sq2.field = DECODE(data, msb, lsb);
+        case 0xff16:
+            NR21;
+            audio->sq2.load_len = 64 - audio->sq2.load_len;
+            break;
+        case 0xff17:
+            NR22;
+            break;
+        case 0xff18:
+            NR23;
+            break;
+        case 0xff19:
+            NR24;
+            if(DECODE(data, 7, 7))
+            {
+                audio->sq2.en = 1;
+                audio->sq2.volume = audio->sq2.start_volume;
+            }
+            break;
+    #undef X
     }
 }
--- src/audio.h
+++ src/audio.h
@@ -2,21 +2,68 @@
 #include "types.h"
 typedef struct cpu_state cpu_state_t;
 
+#define NR10              \
+    X(shift,        2, 0) \
+    X(negate,       3, 3) \
+    X(sweep_period, 6, 4)
+
+#define NR11              \
+    X(load_len,     5, 0) \
+    X(duty,         7, 6) \
+
+#define NR12              \
+    X(period,       2, 0) \
+    X(add_mode,     3, 3) \
+    X(start_volume, 7, 4)
+
+#define NR13              \
+    X(freq_lsb,     7, 0) \
+
+#define NR14              \
+    X(freq_msb,     2, 0) \
+    X(len_en,       6, 6) \
+    X(trigger,      7, 7)
+
+#define NR21              \
+    X(load_len,     5, 0) \
+    X(duty,         7, 6) \
+
+#define NR22              \
+    X(period,       2, 0) \
+    X(add_mode,     3, 3) \
+    X(start_volume, 7, 4)
+
+#define NR23              \
+    X(freq_lsb,     7, 0) \
+
+#define NR24              \
+    X(freq_msb,     2, 0) \
+    X(len_en,       6, 6) \
+    X(trigger,      7, 7)
+
 typedef struct audio
 {
 	cpu_state_t *state;
-    int shift;
-    int negate;
-    int sweep_period;
-    int load_len;
-    int duty;
-    int period;
-    int add_mode;
-    int start_volume;
-    int volume;
-    int freq;
-    int len_en;
-    int en;
+#define X(name, msb, lsb) uint8_t name;// : ((msb - lsb) + 1);
+    struct{
+        NR10
+        NR11
+        NR12
+        NR13
+        NR14
+        int volume;
+        int en;
+    }sq1;
+
+    struct{
+        NR21
+        NR22
+        NR23
+        NR24
+        int volume;
+        int en;
+    }sq2;
+#undef X
 }audio_t;
 
 void     audio_start_thread(void);
@@ -25,3 +72,4 @@ reg_t    audio_load(audio_t *audio, reg16_t addr);
 void     audio_simulate(audio_t *audio, int clk);
 void     audio_store(audio_t *audio, reg16_t addr, reg_t data);
 void     audio_delete(audio_t *audio);
+
