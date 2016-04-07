0e658dde0db103be6660581aa2efe1bfad8f287e
Stuart Jones <stuart@wynjones1.com>
2016-04-08 00:40:23 +0100

    n

--- deps/FindSDL2.cmake
+++ deps/FindSDL2.cmake
@@ -21,23 +21,34 @@ if(MSVC)
 	endif()
 	
 	set(SDL2_LIBRARIES
-		${SDL2_LIBRARY_PATH}/SDL2.lib
-		${SDL2_LIBRARY_PATH}/SDL2main.lib
+        ${SDL2_LIBRARY_PATH}/SDL2.lib
+        ${SDL2_LIBRARY_PATH}/SDL2main.lib
 	)
 else()
     include(FindPkgConfig)
-    pkg_check_modules(SDL2 sdl2)
+    # If we can't find the library then we will
+    # build it from source, this is mainly because
+    # TravisCI does not contain SDL2 in it's repos
+    pkg_check_modules(SDL2 QUIET sdl2)
     if(NOT SDL2_FOUND)
-        message(STATUS "Building SDL2 from source...")
+        message(STATUS "SDL2 needs to be built from source...")
+        # Download and build the libraries.
         include(ExternalProject)
         set(INSTALL_DIR ${CMAKE_BINARY_DIR}/external/)
         ExternalProject_Add(
-            SDL2
-             URL https://www.libsdl.org/release/SDL2-2.0.4.tar.gz
-             CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
+            SDL2_BUILD
+            URL https://www.libsdl.org/release/SDL2-2.0.4.tar.gz
+            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}
         )
-        set( ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${INSTALL_DIR}/lib/pkgconfig")
-        pkg_check_modules(SDL2 sdl2 QUIET)
-        set(SDL2_LIBRARIES ${SDL2_LIBRARY_PATH}/libSDL2.a ${SDL2_LIBRARY_PATH}/libSDL2main.a)
+        # Import the generated libraries into the build system.
+        add_library(SDL2     IMPORTED SHARED)
+        add_library(SDL2main IMPORTED STATIC)
+        set_property(TARGET SDL2     PROPERTY IMPORTED_LOCATION ${INSTALL_DIR}/lib/libSDL2.so)
+        set_property(TARGET SDL2main PROPERTY IMPORTED_LOCATION ${INSTALL_DIR}/lib/libSDL2main.a)
+        set(SDL2_LIBRARIES SDL2 SDL2main)
+
+        # Make sure the libraries are built before we use them.
+        add_dependencies(SDL2     SDL2_BUILD)
+        add_dependencies(SDL2main SDL2_BUILD)
     endif()
 endif()
