1c9cb2d0985ad2097173b2893f4caca8b0475a74
stuart jones <stuart@wynjones1.com>
2016-04-12 20:30:25 +0100

    n

--- tests/blaargg/main.c
+++ tests/blaargg/main.c
@@ -5,10 +5,15 @@
 #ifndef ROM_FILE
 	#error "ROM_FILE must be defined."
 #endif
+
+#ifndef BOOT_ROM
+	#define BOOT_ROM NULL
+#endif
 int main(int argc, char **argv)
 {
 	cmdline_read(argc, argv);
-	cmdline_args.in = ROM_FILE;
+	cmdline_args.in       = ROM_FILE;
+	cmdline_args.boot_rom = BOOT_ROM;
 	debug_init();
 	cpu_state_t *state = cpu_init();
 	cpu_start(state);
